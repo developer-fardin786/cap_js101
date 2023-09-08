5818d1da2daf0c1c97573dda80b454eab2e375cd
developer-fardin786 <developerfardin2.0@gmail.com>
2023-09-08 11:36:53 +0530

    adde masai employee dashbord

--- dom3/PT-WEB-05 C2_905871/index.js
+++ dom3/PT-WEB-05 C2_905871/index.js
@@ -1,8 +1,8 @@
 // fill in javascript code here
 let my_form = document.querySelector("form")
 let user_name = document.querySelector("#name")
-let em_Id = document.querySelector("#employeeID")
-let depertment = document.querySelector("#department")
+let doc_Id = document.querySelector("#dicID")
+let depertment = document.querySelector("#dept")
 let exp = document.querySelector("#exp")
 let email = document.querySelector("#email")
 let contact_num = document.querySelector("#mbl")
@@ -20,7 +20,7 @@ my_form.addEventListener("submit", function (event) {
     } else {
         emp_role = "Fresher"
     }
-
+  
     let data = {
         name: user_name.value,
         employee_ID: em_Id.value,
@@ -29,20 +29,13 @@ my_form.addEventListener("submit", function (event) {
         email: email.value,
         mobile_number: contact_num.value,
         role: emp_role,
-
+    
     }
-    //    console.log(data.mobile_number)/
     user_data.push(data)
+    // console.log(user_data)
     t_body.innerHTML = ""
-    displayData(user_data)
-    // sorting(user_data)
-
-})
-
-
-function displayData(user_datas) {
-    sorting(user_datas)
-    user_datas.map((ele,) => {
+    user_data.map((ele) => {
+        console.log(ele)
         let tr = document.createElement("tr")
         let td1 = document.createElement("td")
         let td2 = document.createElement("td")
@@ -51,7 +44,8 @@ function displayData(user_datas) {
         let td5 = document.createElement("td")
         let td6 = document.createElement("td")
         let td7 = document.createElement("td")
-        let td8 = document.createElement("button")
+        let td8 = document.createElement("td")
+
 
         td1.innerText = ele.name
         td2.innerText = ele.employee_ID
@@ -61,23 +55,9 @@ function displayData(user_datas) {
         td6.innerText = ele.mobile_number
         td7.innerText = ele.role
         td8.innerText = "Delete"
-        td8.addEventListener("click", function removeItem() {
-            let updatedData = user_datas.filter((filter) => filter.employee_ID != ele.employee_ID)
-            user_data = updatedData
-            t_body.innerHTML = ""
-            displayData(user_data)
-        })
-        tr.append(td1, td2, td3, td4, td5, td6, td7, td8)
+        tr.append(td1, td2, td3, td4, td5, td6, td7,td8)
         t_body.append(tr)
-
     })
-}
+})
 
-function sorting(sorting_datas) {
-    sorting_datas.sort((a, b) => {
-        console.log(a.experience, "exp")
-        return a.experience - b.experience
 
-    })
-    console.log(user_data, "user data");
-}
